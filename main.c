7c638237a43ce87e95ac1c8c96c1be611b4539b9
Piotr Francug <piotr.francug@gmail.com>
2018-10-14 22:01:35 +0200

    dodano funckjonalność - kalkulator

--- main.c
+++ main.c
@@ -1,16 +1,25 @@
 #include <stdio.h>
 
+void kalkulator();
+
+void kalkulator_pobierzDane(double *kalkulator_A, double *kalkulator_B, char *kalkulator_znak);
+
+void kalkulator_wyniki(double kalkulator_A, double kalkulator_B, char kalkulator_znak, int *kalkulator_wybor);
+
 void main() {
     int wybor;
-    printf("Witaj w systemie Matematyk 1.0!\nWybierz jeden z dostępnych programów:\n");
-    printf("1. Kalkulator podstawowy\n");
-    printf("2. Delta i miejsca zerowe\n");
-    printf("3. Wyznacznik macierzy drugiego stopnia\n");
-    printf("9. Wyjście\n");
-    printf("Twój wybór: ");
+    printf(
+            "Witaj w systemie Matematyk 1.0!\nWybierz jeden z dostępnych programów:\n"
+            "1. Kalkulator podstawowy\n"
+            "2. Delta i miejsca zerowe\n"
+            "3. Wyznacznik macierzy drugiego stopnia\n"
+            "9. Wyjście\n"
+            "Twój wybór: "
+    );
     scanf(" %i", &wybor);
     if (wybor == 1) {
         printf("Wybrano opcję nr 1");
+        kalkulator();
     } else if (wybor == 2) {
         printf("Wybrano opcję nr 2");
     } else if (wybor == 3) {
@@ -18,9 +27,11 @@ void main() {
     } else if (wybor == 9) {
         printf("Kończenie pracy programu...");
     } else {
-        printf("Wprowadzono nieprawidłowe dane.\nCzy chcesz zacząć od początku?\n");
-        printf("1. Tak!\n");
-        printf("2. Nope.\n");
+        printf(
+                "Wprowadzono nieprawidłowe dane.\nCzy chcesz zacząć od początku?\n"
+                "1. Tak!\n"
+                "2. Nope.\n"
+        );
         wybor = 0;
         scanf(" %i", &wybor);
         if (wybor == 1) {
@@ -28,5 +39,61 @@ void main() {
         } else {
             printf("Kończenie pracy programu...");
         }
+    }
+}
+
+void kalkulator() {
+    double kalkulator_A, kalkulator_B;
+    char kalkulator_znak;
+    int kalkulator_wybor = 0;
+    kalkulator_pobierzDane(&kalkulator_A, &kalkulator_B, &kalkulator_znak);
+    kalkulator_wyniki(kalkulator_A, kalkulator_B, kalkulator_znak, &kalkulator_wybor);
+}
+
+void kalkulator_pobierzDane(double *kalkulator_A, double *kalkulator_B, char *kalkulator_znak) {
+    printf(
+            "Kalkulator podstawowy\n"
+            "Dozwolone działania: dodawanie (+), odejmowanie (-), mnożenie (*), dzielenie (/)\n"
+            "A - pierwsza liczba, B - druga liczba\n"
+    );
+    printf("Podaj A: ");
+    scanf(" %lf", kalkulator_A);
+    printf("Podaj B: ");
+    scanf(" %lf", kalkulator_B);
+    printf("Podaj znak działania: ");
+    scanf(" %c", kalkulator_znak);
+}
+
+void kalkulator_wyniki(double kalkulator_A, double kalkulator_B, char kalkulator_znak, int *kalkulator_wybor) {
+    switch (kalkulator_znak) {
+        case '+' :
+            printf("A + B = %.2lf", kalkulator_A + kalkulator_B);
+            break;
+        case '-' :
+            printf("A - B = %.2lf", kalkulator_A - kalkulator_B);
+            break;
+        case '*' :
+            printf("A * B = %.2lf", kalkulator_A * kalkulator_B);
+            break;
+        case '/' :
+            if (!kalkulator_B) {
+                printf("Nie dziel cholero przez zero!\n");
+                kalkulator();
+            } else {
+                printf("A / B = %.2lf", kalkulator_A / kalkulator_B);
+                break;
+            }
+        default :
+            printf(
+                    "Wprowadzono nieprawidłowe dane.\nCzy chcesz ponownie wprowadzić dane?\n"
+                    "1. Tak!\n"
+                    "2. Nope.\n"
+            );
+            scanf(" %i", kalkulator_wybor);
+            if ((*kalkulator_wybor) == 1) {
+                kalkulator();
+            } else {
+                printf("Kończenie pracy programu...");
+            }
     }
 }
