8c447ef5e9c0b9f5a8a3e3bf912f30e827922813
Piotr Francug <piotr.francug@gmail.com>
2018-10-14 13:20:15 +0200

    empty project

---
+++ cmake-build-debug/CMakeFiles/matematyk.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/matematyk.dir/main.c.o"
+  "matematyk.pdb"
+  "matematyk"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/matematyk.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
